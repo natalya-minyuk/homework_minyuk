a5593a8358651343b6a9ac14dc1dd7bb150fc7a6
natalya-minyuk <natasha-vieru@yandex.ru>
2020-05-28 13:14:53 +0300

    Задание пока не выполняла, пока заглушка

---
+++ src/test/java/ru/geekbrains/main/site/at/BaseTest.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.main.site.at;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+    protected final String BASE_URL = "https://geekbrains.ru";
+
+    @BeforeEach
+    void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
